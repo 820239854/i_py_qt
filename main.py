9f5210a391722185b66e4e4a4eab8884f134922d
Xing <820239854@qq.com>
2022-08-16 16:16:35 +0800

    任意顶层Widget都是Window，但QMainWindow封装好很多东西，直接用更方便

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from PySide6.QtWidgets import QApplication, QWidget
-# Only needed for access to command line arguments
+from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-window = QWidget()
+window = QMainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
 app.exec()
-
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
