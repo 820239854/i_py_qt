afb8a2dc2985c76097a1aa9fdb59bb24c3dac97d
Xing <820239854@qq.com>
2022-08-16 16:59:47 +0800

    chain things together using signals

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 import sys
+from random import choice
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.n_times_clicked = 0
+
         self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
         self.button.clicked.connect(self.the_button_was_clicked)
 
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
         # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
     def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
 
-        # Also change the window title.
-        self.setWindowTitle("My Oneshot App")
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 
 app = QApplication(sys.argv)
