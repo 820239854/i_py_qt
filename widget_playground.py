929ffa0f64e731e69d737bfc95379a3625107cf2
Xing <820239854@qq.com>
2022-08-16 20:24:18 +0800

    QSlider

--- widget_playground.py
+++ widget_playground.py
@@ -1,35 +1,40 @@
 import sys
 
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QSpinBox, QDoubleSpinBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QSlider
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("My App")
 
-        widget = QSpinBox()
-        # widget = QDoubleSpinBox()
+        widget = QSlider(Qt.Vertical)
 
         widget.setMinimum(-10)
         widget.setMaximum(3)
         # Or: widget.setRange(-10,3)
 
-        widget.setPrefix("$")
-        widget.setSuffix("c")
-        widget.setSingleStep(3)  # Or e.g. 0.5 for QDoubleSpinBox
+        widget.setSingleStep(3)
+
         widget.valueChanged.connect(self.value_changed)
-        widget.textChanged.connect(self.value_changed_str)
+        widget.sliderMoved.connect(self.slider_position)
+        widget.sliderPressed.connect(self.slider_pressed)
+        widget.sliderReleased.connect(self.slider_released)
 
         self.setCentralWidget(widget)
 
     def value_changed(self, i):
         print(i)
-    def value_changed_str(self, s):
-        print(s)
 
+    def slider_position(self, p):
+        print("position", p)
+
+    def slider_pressed(self):
+        print("Pressed!")
+
+    def slider_released(self):
+        print("Released")
 
 app = QApplication(sys.argv)
 
