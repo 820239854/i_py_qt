9d79dd5a7cac869b54f9c0121ddcb68d9cca3669
Xing <820239854@qq.com>
2022-08-16 16:21:00 +0800

    继承QMainWindow居中放置Button

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window = MainWindow()
+window.show()
 
-# Start the event loop.
-app.exec()
+app.exec_()
