f9f2204376ad42a7f845e4994b2ff678af269e98
Xing <820239854@qq.com>
2022-08-16 16:39:35 +0800

    store the current state of a widget in a Python variable

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
 
         button = QPushButton("Press Me!")
         button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
         button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
-
     def the_button_was_toggled(self, checked):
-        print("Checked?", checked)
+        self.button_is_checked = checked
+
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
