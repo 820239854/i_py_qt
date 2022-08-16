97940c7563bd5db52d9b1d8019af881ca30fd0ec
Xing <820239854@qq.com>
2022-08-16 16:50:07 +0800

    Changing the interface

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
 import sys
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.button_is_checked = True
-
         self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_released)
-        self.button.setChecked(self.button_is_checked)
+        self.button.clicked.connect(self.the_button_was_clicked)
 
         # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
-    def the_button_was_released(self):
-        self.button_is_checked = self.button.isChecked()
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
 
-        print(self.button_is_checked)
+        # Also change the window title.
+        self.setWindowTitle("My Oneshot App")
+
 
 app = QApplication(sys.argv)
 
