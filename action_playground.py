762f2787a26523664de5129202f9fd4907ef12fb
Xing <820239854@qq.com>
2022-08-17 10:50:30 +0800

    QToolBar add a statusbar

--- action_playground.py
+++ action_playground.py
@@ -2,10 +2,7 @@ import sys
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QToolBar, )
-
-from layout_colorwidget import Color
-
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QToolBar, QStatusBar, )
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,21 +12,20 @@ class MainWindow(QMainWindow):
 
         label = QLabel("Hello!")
         label.setAlignment(Qt.AlignCenter)
-
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
         self.addToolBar(toolbar)
 
         button_action = QAction("Your button", self)
-        # this text will be displayed on the status bar once we have one
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         toolbar.addAction(button_action)
 
+        self.setStatusBar(QStatusBar(self))
+
     def onMyToolBarButtonClick(self, s):
         print("click", s)
-
 
 app = QApplication(sys.argv)
 
