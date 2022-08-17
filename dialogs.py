6286813e1a6d6949d9b205c624ec8327c4130f3f
Xing <820239854@qq.com>
2022-08-17 14:57:18 +0800

    Init CustomDialog

--- dialogs.py
+++ dialogs.py
@@ -1,6 +1,24 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QLabel, QVBoxLayout
+
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HELLO!")
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 
 class MainWindow(QMainWindow):
@@ -16,11 +34,11 @@ class MainWindow(QMainWindow):
     def button_clicked(self, s):
         print("click", s)
 
-        dlg = QDialog(self)
-        dlg.setWindowTitle("?")
+        dlg = CustomDialog()
 
-        # we start it using.exec() - just like we did for QApplication to create the main event loop of our application.
-        # That’s not a coincidence: when you exec the QDialog an entirely new event loop - specific for the dialog - is created.
+        # we start it using.exec() - just like we did for QApplication to create the main event loop of our
+        # application. That’s not a coincidence: when you exec the QDialog an entirely new event loop - specific for
+        # the dialog - is created.
         dlg.exec()
 
 
