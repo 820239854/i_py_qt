d7ca74811d597bd1c2348d678e84c1ac0f22b548
Xing <820239854@qq.com>
2022-08-17 11:09:59 +0800

    Init Menus

--- action_playground.py
+++ action_playground.py
@@ -12,13 +12,14 @@ class MainWindow(QMainWindow):
 
         label = QLabel("Hello!")
         label.setAlignment(Qt.AlignCenter)
+
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon("./res/bug.png"), "Your button", self)
+        button_action = QAction(QIcon("./res/bug.png"), "&Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
@@ -26,7 +27,7 @@ class MainWindow(QMainWindow):
 
         toolbar.addSeparator()
 
-        button_action2 = QAction(QIcon("./res/bug.png"), "Your button2", self)
+        button_action2 = QAction(QIcon("./res/bug.png"), "Your &button2", self)
         button_action2.setStatusTip("This is your button2")
         button_action2.triggered.connect(self.onMyToolBarButtonClick)
         button_action2.setCheckable(True)
@@ -36,6 +37,12 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
+
+        menu = self.menuBar()
+
+        # The ampersand defines a quick key to jump to this menu when pressing Alt.
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(button_action)
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
