d89c55510a36488607fe7b7033a40bc289b8061a
Xing <820239854@qq.com>
2022-08-22 21:14:46 +0800

    本地读取JSON，进行界面持久化

--- mvcToDo.py
+++ mvcToDo.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from PySide6 import QtCore, QtGui, QtWidgets
@@ -32,6 +33,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.model = TodoModel(todos=[(False, 'my first todo')])
+        self.load()
+
         self.todoView.setModel(self.model)
 
         self.addButton.pressed.connect(self.add)
@@ -52,6 +55,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.model.layoutChanged.emit()
             # Empty the input
             self.todoEdit.setText("")
+            self.save()
 
     def delete(self):
         indexes = self.todoView.selectedIndexes()
@@ -63,6 +67,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.model.layoutChanged.emit()
             # Clear the selection (as it is no longer valid).
             self.todoView.clearSelection()
+            self.save()
 
     def complete(self):
         indexes = self.todoView.selectedIndexes()
@@ -76,7 +81,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.model.dataChanged.emit(index, index)
             # Clear the selection (as it is no longer valid).
             self.todoView.clearSelection()
+            self.save()
 
+    def load(self):
+        try:
+            with open('data.json', 'r') as f:
+                self.model.todos = json.load(f)
+        except Exception:
+            pass
+
+    def save(self):
+        with open('data.json', 'w') as f:
+            data = json.dump(self.model.todos, f)
 
 
 app = QtWidgets.QApplication(sys.argv)
