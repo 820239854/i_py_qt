6d669b78b8f1d3126e36867b3abe0bc107a22090
Xing <820239854@qq.com>
2022-08-22 21:54:03 +0800

    TableModel进一步定制化

--- mvc_table.py
+++ mvc_table.py
@@ -3,13 +3,50 @@ from datetime import datetime
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
 
+COLORS = ['#053061', '#2166ac', '#4393c3', '#92c5de', '#d1e5f0', '#f7f7f7', '#fddbc7', '#f4a582', '#d6604d',
+          '#b2182b', '#67001f']
 
 class TableModel(QtCore.QAbstractTableModel):
+
     def __init__(self, data):
         super().__init__()
         self._data = data
 
     def data(self, index, role):
+        if role == Qt.DecorationRole:
+            value = self._data[index.row()][index.column()]
+            if isinstance(value, bool):
+                if value:
+                    return QtGui.QIcon("mvc_ui/tick.png")
+
+                return QtGui.QIcon("mvc_ui/cross.png")
+
+        if role == Qt.ForegroundRole:
+            value = self._data[index.row()][index.column()]
+
+            if (isinstance(value, int) or isinstance(value, float)) and value < 0:
+                return QtGui.QColor("red")
+
+        if role == Qt.TextAlignmentRole:
+            value = self._data[index.row()][index.column()]
+
+            if isinstance(value, int) or isinstance(value, float):
+                # Align right, vertical middle.
+                return Qt.AlignVCenter | Qt.AlignRight
+
+        if role == Qt.BackgroundRole:
+            value = self._data[index.row()][index.column()]
+            if isinstance(value, int) or isinstance(value, float):
+                value = int(value)  # Convert to integer for indexing.
+
+                # Limit to range -5 ... +5, then convert to 0..10
+                value = max(-5, value)  # values < -5 become -5
+                value = min(5, value)  # valaues > +5 become +5
+                value = value + 5  # -5 becomes 0, +5 becomes + 10
+
+                return QtGui.QColor(COLORS[value])
+
+
         if role == Qt.DisplayRole:
             # Get the raw value
             value = self._data[index.row()][index.column()]
@@ -47,11 +84,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.table = QtWidgets.QTableView()
 
         data = [
-            [4, 9, 2],
-            [1, -1, 'hello'],
-            [3.023, 5, -5],
-            [3, 3, datetime(2017, 10, 1)],
-            [7.555, 8, 9],
+            [True, 9, 2],
+            [1, 0, -1],
+            [3, 5, False],
+            [3, 3, 2],
+            [datetime(2019, 5, 4), 8, 9],
         ]
 
         self.model = TableModel(data)
