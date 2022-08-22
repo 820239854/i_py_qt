12e4961280f3b6e093e22a4c4c58b407ad46a6d8
Xing <820239854@qq.com>
2022-08-22 20:46:57 +0800

    初始化Model并绑定

--- mvcToDo.py
+++ mvcToDo.py
@@ -3,13 +3,31 @@ import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
 
+
 from mvc_ui.MainWindow import Ui_MainWindow
+
+# tag::model[]
+class TodoModel(QtCore.QAbstractListModel):
+    def __init__(self, todos=None):
+        super().__init__()
+        self.todos = todos or []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            status, text = self.todos[index.row()]
+            return text
+
+    def rowCount(self, index):
+        return len(self.todos)
+# end::model[]
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.model = TodoModel(todos=[(False, 'my first todo')])
+        self.todoView.setModel(self.model)
 
 
 app = QtWidgets.QApplication(sys.argv)
